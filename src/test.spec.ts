629885f4716c116887d5ad89e88d33f1a939e810
Rich Somerfield <rs@richsomerfield.com>
2016-09-05 10:13:10 +0100

    Separating out tests

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -5,22 +5,32 @@ let expect = chai.expect;
 
 import * as R from 'ramda-ts';
 
-describe('basic', () => {
-    it('1', () => {
-        let result;
+const add = (accum: number, value: number): number => {return accum + value};
+const double = (x: number): number => { return x * 2 };
+const isEven = (x: number): boolean => { return x % 2 === 0 };
 
-        const double = (x: number) => { return x * 2 };
-        result = R.map(double, [1, 2, 3]);
+describe('basic', () => {
+    it('map', () => {
+        let result = R.map(double, [1, 2, 3]);
         console.log(result);
         expect(result).to.deep.equal([2, 4, 6]);
+    });
 
-        const isEven = (x: number) => { return x % 2 === 0 };
-        result = R.filter(isEven, [1, 2, 3, 4]);
+    it('filter', () => {
+        let result = R.filter(isEven, [1, 2, 3, 4]);
         console.log(result);
         expect(result).to.deep.equal([2,4]);
+    });
 
-        result = R.reject(isEven, [1, 2, 3, 4]);
+    it('reject', () => {
+        let result = R.reject(isEven, [1, 2, 3, 4]);
         console.log(result);
-        expect(result).to.deep.equal([1,3]);
+        expect(result).to.deep.equal([1, 3]);
+    });
+
+    it('add', () => {
+        let result = R.reduce(add, 5, [1,2,3,4]);
+        console.log(result);
+        expect(result).to.equal(15);
     });
 });
